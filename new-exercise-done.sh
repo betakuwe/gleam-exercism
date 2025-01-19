492c5b16c2583817147db363f7eadbbca5149c88
Ben <betakuwe@gmail.com>
2025-01-19 15:59:29 +0800

    more interactive script

--- new-exercise-done.sh
+++ new-exercise-done.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+set -euo pipefail
 git add .
-git commit -m "$(basename $(pwd))"
-git push
+git status
+COMMIT_MSG="$(basename "$(pwd)")"
+echo "Commit with message: $COMMIT_MSG"
+read -rp "Proceed?(y/n)" PROCEED
+if [[ $PROCEED = "y" ]]; then
+	git commit -m "$COMMIT_MSG"
+	git push
+fi
