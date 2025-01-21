5f51986a088e746144862473c75b3578f26e3a0f
Ben <betakuwe@gmail.com>
2025-01-22 06:33:50 +0800

    make auto push script simpler

--- new-exercise-done.sh
+++ new-exercise-done.sh
@@ -2,10 +2,5 @@
 set -euo pipefail
 git add .
 git status
-COMMIT_MSG="$(basename "$(pwd)")"
-echo "Commit with message: $COMMIT_MSG"
-read -rp "Proceed?(y/n)" PROCEED
-if [[ $PROCEED = "y" ]]; then
-	git commit -m "$COMMIT_MSG"
-	git push
-fi
+git commit -m "$(basename "$(pwd)")"
+git push
